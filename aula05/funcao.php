f9deb45746f55efb10918a17e9c0223c3e003378
Bruna de Mello <bubumellopinheiro@gmail.com>
2025-04-09 10:32:59 -0300

    aula função php

---
+++ aula05/funcao.php
@@ -0,0 +1,19 @@
+<?php
+    function myMessage() {
+        echo "Hello World! <br>";
+    }
+    
+    myMessage();
+    myMessage();
+
+    function Soma($n1, $n2) {
+        echo $n1 + $n2 , "<br>";
+    }
+
+    Soma(3, 4);
+    Soma(3, 10);
+
+    function cadastrar() {
+        //teria uma lógica para um cadastro
+    }
+?>
