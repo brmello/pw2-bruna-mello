fe4ab5bae9c772b0a6b9a54703e97f355086ac9f
Bruna de Mello <bubumellopinheiro@gmail.com>
2025-05-14 10:32:42 -0300

    exercicios php

---
+++ exercicios/ex1.php
@@ -0,0 +1,8 @@
+<html>
+    <body>
+
+        Sua distancia total é <?php echo $_POST["distancia"]; ?> <br>
+        Sua quantidade de litro total é <?php echo $_POST["litros"]; ?>
+
+    <body>
+<html>
