1db022ae1b07b41235d42f5085315ba5269b84f5
Bruna de mello <bubumellopinheiro@gmail.com>
2025-06-09 08:09:43 -0300

    atualizaçao

---
+++ aula01/olamundo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <style>
+        .texto {
+            size:5ram;
+        }
+    </style>
+
+    <body>
+    <p class="texto">
+        <?php
+           echo "Hello World!";
+        ?>
+    </p>
+    </body>
+</html>
---
+++ aula02/concatenar.php
@@ -0,0 +1,16 @@
+<?php
+     ECHO "Hello World!<br>";
+     echo "Hello World!<br>";
+     EcHO "Hello World!<br>";
+
+     $nome = "LIL";
+     $sobrenome = "Zé";
+     $numero = 4;
+
+    echo $nome .  "<br>";
+    echo "${nome} <h1> ${sobrenome} </h1>";
+    echo $numero . "<p>" . $nome .
+    " " . $sobrenome . "</p>";
+ 
+
+?>
