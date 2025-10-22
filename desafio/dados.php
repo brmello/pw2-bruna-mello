eea3c0221208ed5cf99d6385450b35ae43603040
bubumellopinheiro@gmail.com <bubumellopinheiro@gmail.com>
2025-10-22 10:34:26 -0300

    melhorias e tela do login

--- desafio/dados.php
+++ desafio/dados.php
@@ -6,6 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
     <link rel="stylesheet" href="style.css?v=2">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
 </head>
 
 <body>
@@ -21,6 +25,15 @@
 
             <label for="idade">Idade:</label><br>
             <input type="number" id="idade" name="idade" required><br><br>
+
+            <label for="endereco">Endereço:</label><br>
+            <input type="text" id="endereco" name="endereco" required><br><br>
+
+            <label for="celular">Celular:</label><br>
+            <input type="number" id="celular" name="celular" required maxlength="11" placeholder="11912345678"><br><br>
+
+            <label for="email">Email:</label><br>
+            <input type="text" id="email" name="email" required><br><br>
 
             <button type="submit" class="btn">Enviar</button>
         </form>
--- desafio/recebe.php
+++ desafio/recebe.php
@@ -4,8 +4,11 @@ include("db.php");
 $nome = $_POST['nome'];
 $cpf = $_POST['cpf'];
 $idade = $_POST['idade'];
+$endereco = $_POST['endereco'];
+$celular = $_POST['celular'];
+$email = $_POST['email'];
 
-$sql = "INSERT INTO dados (nome, cpf, idade) VALUES ('$nome', '$cpf', '$idade')";
+$sql = "INSERT INTO demo (nome, cpf, idade, endereco, celular, email) VALUES ('$nome', '$cpf', '$idade', '$endereco', '$celular', '$email')";
 
 if ($conn->query($sql) === TRUE) {
     echo("Enviado com sucesso! ;)");
